372a0b45bab8e317b20019d00423e64d272faac0
Benjamin Whitman <benjamin.whitman90@gmail.com>
2020-02-12 23:55:44 -0500

    added html files

--- twitoff/__init__.py
+++ twitoff/__init__.py
@@ -1,4 +1,4 @@
 """Entry point for TwitOff."""
-from .app import create_app
+# from .app import create_app
 
-APP = create_app()
+# APP = create_app()
--- twitoff/app.py
+++ twitoff/app.py
@@ -1,11 +1,53 @@
-from flask import Flask
+from flask import Flask, jsonify, request, render_template
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
-def create_app():
-    """Create and configure an instance of the Flask application."""
-    app = Flask(__name__)
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URL"] = "sqlite:///lambdata1.db"
 
-    @app.route('/')
-    def root():
-        return 'Hello TwitOff!'
+db = SQLAlchemy(app)
 
-    return app
+migrate = Migrate(app, db)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(128))
+
+class Tweet(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    status = db.Column(db.String)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/about")
+def about():
+    return "About Me"
+
+@app.route("/users")
+@app.route("/users.json")
+def users():
+    users = User.query.all()
+    print(len(users))
+
+    return jsonify(users)
+
+@app.route("/users/create", methods=["POST"])
+def create_user():
+    print("CREATING A NEW USER...")
+    print("FORM DATA:", dict(request.form))
+
+    if "name" in request.form:
+        name = request.form["name"]
+        print(name)
+        db.session.add(User(name=name))
+        db.session.commit()
+        return jsonify({"message": "CREATED OK", "name": name})
+    else:
+        return jsonify({"message": "OOPS PLEASE SPECIFY A NAME!"})
+
+    db.session.add(User(name=name))
+    db.session.commit()
