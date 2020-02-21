ff65226ec376270c237fe461ab763f21923d5e7d
Benjamin Whitman <benjamin.whitman90@gmail.com>
2020-02-21 17:44:51 -0500

    change datatypes on db to be bigints

--- twitoff/models.py
+++ twitoff/models.py
@@ -6,14 +6,14 @@ db = SQLAlchemy(session_options={"autoflush": False})
 migrate = Migrate()
 
 class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.BigInteger, primary_key=True)
     screen_name = db.Column(db.String(128))
     followers_count = db.Column(db.Integer)
     latest_tweet_id = db.Column(db.BigInteger)
 
 class Tweet(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
+    id = db.Column(db.BigInteger, primary_key=True)
+    user_id = db.Column(db.BigInteger, db.ForeignKey("user.id"))
     full_text = db.Column(db.String(500))
     embedding = db.Column(db.PickleType)
 
